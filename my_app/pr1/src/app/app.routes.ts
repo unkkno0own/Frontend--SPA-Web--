73e3a97e9d58fe8148dfc89fa4ee9d22f18d3bce
Anya <annagalicenko2004@gmail.com>
2025-12-16 20:18:13 +0200

    modified:   my_app/pr1/src/app/app.html 	modified:   my_app/pr1/src/app/app.routes.ts 	new file:   my_app/pr1/src/app/layout.html 	new file:   my_app/pr1/src/app/layout.scss 	new file:   my_app/pr1/src/app/layout.ts

--- my_app/pr1/src/app/app.routes.ts
+++ my_app/pr1/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent
+  }
+];
