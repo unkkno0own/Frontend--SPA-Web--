e0a3cf2584a03f1846e3e9e6c823839dcb9365ff
Anya <annagalicenko2004@gmail.com>
2025-12-22 18:01:09 +0200

    Changes to be committed: 	modified:   my_app/pr1/src/app/components/item-card/item-card.component.scss 	modified:   my_app/pr1/src/app/components/items-list/items-list.component.scss 	modified:   my_app/pr1/src/app/components/items-list/items-list.component.ts 	modified:   my_app/pr1/src/index.html 	modified:   my_app/pr1/src/styles.scss

--- my_app/pr1/src/app/components/items-list/items-list.component.ts
+++ my_app/pr1/src/app/components/items-list/items-list.component.ts
@@ -12,8 +12,8 @@ import { ItemCardComponent } from '../item-card/item-card.component';
 })
 export class ItemsListComponent {
   public products: Product[] = [
-    { id: 1, name: 'Смартфон X10', price: 15000, description: '...', imageUrl: '...', category: '...', inStock: true, isNew: true },
-    { id: 2, name: 'Ноутбук Pro 15', price: 45000, description: '...', imageUrl: '...', category: '...', inStock: false, isNew: false },
-    { id: 3, name: 'Навушники Air', price: 3000, description: '...', imageUrl: '...', category: '...', inStock: true, isNew: true }
+    { id: 1, name: 'Смартфон X10', price: 15000, description: '...', imageUrl: 'https://static5.depositphotos.com/1005574/413/v/600/depositphotos_4139575-stock-illustration-mobile-phone.jpghttps://st.depositphotos.com/1456491/3656/v/600/depositphotos_36569557-stock-illustration-laptop-isolated-on-white.jpg', category: '...', inStock: true, isNew: true },
+    { id: 2, name: 'Ноутбук Pro 15', price: 45000, description: '...', imageUrl: 'https://st.depositphotos.com/1456491/3656/v/600/depositphotos_36569557-stock-illustration-laptop-isolated-on-white.jpg', category: '...', inStock: false, isNew: false },
+    { id: 3, name: 'Навушники Air', price: 3000, description: '...', imageUrl: 'https://st3.depositphotos.com/32514642/37145/i/600/depositphotos_371458236-stock-photo-new-airpods-pro-features-active.jpg', category: '...', inStock: true, isNew: true }
   ];
 }
