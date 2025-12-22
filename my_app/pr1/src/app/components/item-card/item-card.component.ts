f10aee1246d42bc52a684ec4ad40f15119082d00
Anya <annagalicenko2004@gmail.com>
2025-12-22 18:52:23 +0200

    modified:   my_app/pr1/src/app/components/item-card/item-card.component.html 	modified:   my_app/pr1/src/app/components/item-card/item-card.component.scss 	modified:   my_app/pr1/src/app/components/item-card/item-card.component.ts 	modified:   my_app/pr1/src/app/components/items-list/items-list.component.html 	modified:   my_app/pr1/src/app/components/items-list/items-list.component.scss 	modified:   my_app/pr1/src/app/components/items-list/items-list.component.ts

--- my_app/pr1/src/app/components/item-card/item-card.component.ts
+++ my_app/pr1/src/app/components/item-card/item-card.component.ts
@@ -1,14 +1,20 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Product } from '../../shared/models/product.interface';
 
 @Component({
   selector: 'app-item-card',
   standalone: true,
-  imports: [CommonModule], // Для *ngIf та [ngClass]
+  imports: [CommonModule],
   templateUrl: './item-card.component.html',
   styleUrl: './item-card.component.scss'
 })
 export class ItemCardComponent {
-  @Input() item!: Product;
+  @Input({ required: true }) item!: Product;
+
+  @Output() selectItem = new EventEmitter<Product>();
+
+  onDetailsClick(): void {
+    this.selectItem.emit(this.item);
+  }
 }
--- my_app/pr1/src/app/components/items-list/items-list.component.ts
+++ my_app/pr1/src/app/components/items-list/items-list.component.ts
@@ -1,19 +1,31 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common'; // Обов'язково для *ngFor
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'; // Обов'язково для [(ngModel)]
 import { Product } from '../../shared/models/product.interface';
 import { ItemCardComponent } from '../item-card/item-card.component';
 
 @Component({
   selector: 'app-items-list',
   standalone: true,
-  imports: [CommonModule, ItemCardComponent],
+  imports: [CommonModule, FormsModule, ItemCardComponent],
   templateUrl: './items-list.component.html',
   styleUrl: './items-list.component.scss'
 })
 export class ItemsListComponent {
+  public searchText: string = '';
   public products: Product[] = [
     { id: 1, name: 'Смартфон X10', price: 15000, description: '...', imageUrl: 'https://static5.depositphotos.com/1005574/413/v/600/depositphotos_4139575-stock-illustration-mobile-phone.jpghttps://st.depositphotos.com/1456491/3656/v/600/depositphotos_36569557-stock-illustration-laptop-isolated-on-white.jpg', category: '...', inStock: true, isNew: true },
     { id: 2, name: 'Ноутбук Pro 15', price: 45000, description: '...', imageUrl: 'https://st.depositphotos.com/1456491/3656/v/600/depositphotos_36569557-stock-illustration-laptop-isolated-on-white.jpg', category: '...', inStock: false, isNew: false },
-    { id: 3, name: 'Навушники Air', price: 3000, description: '...', imageUrl: 'https://st3.depositphotos.com/32514642/37145/i/600/depositphotos_371458236-stock-photo-new-airpods-pro-features-active.jpg', category: '...', inStock: true, isNew: true }
   ];
+
+  get filteredProducts(): Product[] {
+    return this.products.filter(p => 
+      p.name.toLowerCase().includes(this.searchText.toLowerCase())
+    );
+  }
+
+  handleItemSelected(product: Product): void {
+    console.log('Інформація про обраний елемент:', product);
+    alert(`Ви переглядаєте: ${product.name}`);
+  }
 }
